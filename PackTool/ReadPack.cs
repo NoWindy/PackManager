c27bc30c128a3497ba437d80876dcacdab2e5231
adminuser <nowindnowind@outlook.com>
2018-08-29 11:14:13 +0800

    ver

--- PackTool/ReadPack.cs
+++ PackTool/ReadPack.cs
@@ -1,8 +1,24 @@
 ﻿using System;
-namespace PackTool
+using System.Resources;
+using System.Collections;
+
+namespace ReadPack
 {
     public class ReadPack
     {
-
+        public static byte[] GetFile(string resName, string packPath)
+        {
+            byte[] result = null;
+            using (ResourceReader rr = new ResourceReader(packPath))
+            {
+                foreach (DictionaryEntry entry in rr)
+                    if ((string)entry.Key == resName)
+                    {
+                        result = entry.Value as byte[];
+                        break;
+                    }
+            }
+            return result;
+        }
     }
 }
